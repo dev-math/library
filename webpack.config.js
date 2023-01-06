8ab063f03ecc8a2205797a0674a35a0a05c0d85d
Matheus Rocha <01devmath@gmail.com>
2023-01-06 02:51:55 -0300

    feat: webpack-dev-server serve files on dist dir

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 const config = {
   entry: './src/index.jsx',
   output: {
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
   resolve: {
@@ -32,6 +32,9 @@ const config = {
       directory: path.join(__dirname, 'public')
     },
     open: true,
+    devMiddleware: {
+      publicPath: '/dist',
+    },
   }
 };
 
