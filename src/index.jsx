e1386fc2528d4d1c7663d976eb69007c31cbd7be
Matheus Rocha <01devmath@gmail.com>
2023-02-09 21:43:48 -0300

    feat: add body svg decoration

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { Router } from './routes'
 import './style.css';
+import './utils/top-decoration.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
