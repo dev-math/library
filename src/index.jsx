684b0497d4e0b468b3f48c2505639eb6f3974ac4
Matheus Rocha <01devmath@gmail.com>
2023-02-14 15:37:36 -0300

    fix: wrong import file path

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,15 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { Router } from '@/routes'
+import { AuthProvider } from '@/context/AuthContext';
+
 import '@/style.css';
-import '@/utils/top-decoration.css'
+import '@/assets/top-decoration.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router />
+    <AuthProvider>
+      <Router />
+    </AuthProvider>
   </StrictMode>
 );
